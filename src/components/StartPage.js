81059ece5b7608703bf16e1911c6ec5532f7d588
Rushikesh Kshirsagar <108231382+kshirsagarrushi@users.noreply.github.com>
2023-08-04 02:41:57 +0530

    conditional rendering of start button

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function StartPage() {
     const navigate=useNavigate();
+    const [name,setName]=useState("");
+
     function goToMain(){
         navigate('/main');
     }
+    
   return (
     <div className="start">
         <h1>Responsive Quiz App</h1>
-        <input type="text" className="input-name" placeholder="Enter your name"/>
-        <button className="start-button" onClick={goToMain}>Start</button>
+        <input 
+        type="text" 
+        className="input-name" 
+        placeholder="Enter your name"
+        value={name}
+        onChange={(e)=>setName(e.target.value)}
+        />
+        {
+          name?
+          <button className="start-button" onClick={goToMain}>Start</button>
+          :<h3>Please Enter Your Name!!</h3>
+        }
     </div>
   )
 }
