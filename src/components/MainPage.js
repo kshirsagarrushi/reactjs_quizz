b1cf9a65dc7575ac33185362baa5de07bde73233
Rushikesh Kshirsagar <108231382+kshirsagarrushi@users.noreply.github.com>
2023-08-04 15:05:16 +0530

    randomness of question

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,36 +1,52 @@
 import React, { useState } from "react";
 import Questions from "../Questions";
-import FinalResult from "./FinalResult";
+import { useNavigate } from "react-router-dom";
+// currentQuestion=key
 
 function MainPage() {
   const [result, setResult] = useState(false);
-  const[currentQuestion, setCurrentQuestion]=useState(0);
-  const[score,setScore]=useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [counter,setCounter]=useState(0);
+  const [score, setScore] = useState(0);
+  const navigate = useNavigate();
 
-  function optionClicked(isCorrect){
+  function optionClicked(isCorrect) {
     if (isCorrect) {
       setScore(score + 1);
     }
 
-    if (currentQuestion + 1 < Questions.length) {
-      setCurrentQuestion(currentQuestion + 1);
+    if (counter + 1 < Questions.length) {
+
+      const remainingQuestions = [...Questions].filter(
+        (question, index) => index !== currentQuestion
+      );
+      if (remainingQuestions.length > 0) {
+        const randomIndex = Math.floor(
+          Math.random() * remainingQuestions.length
+        );
+        setCurrentQuestion(randomIndex);
+        setCounter(counter+1);
+      } else {
+        setResult(true);
+      }
     } else {
       setResult(true);
     }
   }
+
+  function goToFinalResult() {
+    navigate("/result", { state: { score: score } });
+  }
   return (
     <div className="main-page">
-    {result?
-      <FinalResult
-        score={score}
-        question={Questions}
-      />
-      :
-      <div className="question-card">
-        <h2>Question {currentQuestion+1} out of 5</h2>
-        <h2>{Questions[currentQuestion].text}</h2>
-        <ul>
-        {Questions[currentQuestion].options.map((option) => {
+      {result ? (
+        goToFinalResult()
+      ) : (
+        <div className="question-card">
+          <h2>Question {counter + 1} out of 5</h2>
+          <h2>{Questions[currentQuestion].text}</h2>
+          <ul>
+            {Questions[currentQuestion].options.map((option) => {
               return (
                 <li
                   key={option.id}
@@ -40,10 +56,9 @@ function MainPage() {
                 </li>
               );
             })}
-        </ul>
-      </div>
-      }   
-      
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
